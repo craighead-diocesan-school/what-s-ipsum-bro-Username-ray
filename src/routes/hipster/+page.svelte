110b6b24623f458c1f2e77c8f81d29db12a4b3c8
Username-ray <maedara96@craighead.school.nz>
2024-03-05 14:21:51 +1300

    Curved h2 letters

--- src/routes/hipster/+page.svelte
+++ src/routes/hipster/+page.svelte
@@ -9,16 +9,63 @@
 <Nav />
 
 <main>
-  <h2>You Probably Haven't Heard of Hipster Ipsum</h2>
+  <h2>
+    <div class="warp" aria-labelledby="warp-label">
+      <span id="warp-label" class="warp__placeholder"> You Probably Haven't Heard of Hipster Ipsum. </span>
+
+      <span role="presentation">
+        <span class="warp__0">Y</span>
+        <span class="warp__1">o</span>
+        <span class="warp__2">u</span>
+        <span class="warp__3"> </span>
+        <span class="warp__4">P</span>
+        <span class="warp__5">r</span>
+        <span class="warp__6">o</span>
+        <span class="warp__7">b</span>
+        <span class="warp__8">a</span>
+        <span class="warp__9">b</span>
+        <span class="warp__10">l</span>
+        <span class="warp__11">y</span>
+        <span class="warp__12"> </span>
+        <span class="warp__13">H</span>
+        <span class="warp__14">a</span>
+        <span class="warp__15">v</span>
+        <span class="warp__16">e</span>
+        <span class="warp__17">n</span>
+        <span class="warp__18">'</span>
+        <span class="warp__19">t</span>
+        <span class="warp__20"> </span>
+        <span class="warp__21">H</span>
+        <span class="warp__22">e</span>
+        <span class="warp__23">a</span>
+        <span class="warp__24">r</span>
+        <span class="warp__25">d</span>
+        <span class="warp__26"> </span>
+        <span class="warp__27">o</span>
+        <span class="warp__28">f</span>
+        <span class="warp__29"> </span>
+        <span class="warp__30">H</span>
+        <span class="warp__31">i</span>
+        <span class="warp__32">p</span>
+        <span class="warp__33">s</span>
+        <span class="warp__34">t</span>
+        <span class="warp__35">e</span>
+        <span class="warp__36">r</span>
+        <span class="warp__37"> </span>
+        <span class="warp__38">I</span>
+        <span class="warp__39">p</span>
+        <span class="warp__40">s</span>
+        <span class="warp__41">u</span>
+        <span class="warp__42">m</span>
+        <span class="warp__43">.</span>
+      </span>
+    </div>
+  </h2>
 
   <div class="container">
     <p class="paragraph1">Fingerstache tofu pickled vibecession kitsch lyft blackbird spyplane vegan craft beer keytar banh mi thundercats. Hashtag heirloom vape scenester paleo hexagon. Blackbird spyplane pok pok DIY heirloom scenester food truck kale chips pabst, bitters raw denim artisan waistcoat seitan freegan.</p>
 
     <p class="paragraph2">Put a bird on it vibecession fashion axe YOLO asymmetrical. VHS photo booth listicle fam truffaut subway tile yr cred chicharrones semiotics biodiesel green juice microdosing whatever. Truffaut praxis PBR and B tattooed street art. Prism jean shorts pickled vegan. Vegan praxis cronut kitsch pop-up. Letterpress street art mlkshk marxism.</p>
-
-    <div class="image">
-      <img src="man.jpg" alt="Hipster" />
-    </div>
   </div>
 </main>
 
@@ -28,12 +75,254 @@
 
 <style>
   main {
-    background: linear-gradient(45deg, #2cd9cd 0%, #c5c1ff 56%, #f3bccc 100%);
+    background-image: url("afloimagemart_238706957.webp");
+    background-size: cover;
     font-family: "Barriecito", system-ui;
     padding-bottom: 20px;
     font-size: 120%;
   }
-  h2 {
+  /* Proudly created with www.csswarp.com */
+
+  .warp__placeholder {
+    position: absolute;
+    color: transparent;
+    font-size: 0.1px;
+  }
+
+  .warp {
+    display: block;
+    position: relative;
+    width: 1021px;
+    height: 625px;
+    font: normal 33px/1 Barriecito;
+  }
+
+  [class*="warp__"] {
+    display: block;
+    position: absolute;
+  }
+
+  .warp__0 {
+    transform-origin: 50% 28px;
+    transform: translate(64.6122px, 318.1162px) rotate(0.007784rad);
+  }
+
+  .warp__1 {
+    transform-origin: 50% 28px;
+    transform: translate(85.6635px, 317.4231px) rotate(-0.071438rad);
+  }
+
+  .warp__2 {
+    transform-origin: 50% 28px;
+    transform: translate(108.2771px, 314.8983px) rotate(-0.149929rad);
+  }
+
+  .warp__3 {
+    transform-origin: 50% 28px;
+    transform: translate(131.1024px, 310.6882px) rotate(-0.221671rad);
+  }
+
+  .warp__4 {
+    transform-origin: 50% 28px;
+    transform: translate(152.6444px, 304.9529px) rotate(-0.286166rad);
+  }
+
+  .warp__5 {
+    transform-origin: 50% 28px;
+    transform: translate(174.6354px, 298.3052px) rotate(-0.340104rad);
+  }
+
+  .warp__6 {
+    transform-origin: 50% 28px;
+    transform: translate(193.384px, 290.5821px) rotate(-0.387548rad);
+  }
+
+  .warp__7 {
+    transform-origin: 50% 28px;
+    transform: translate(214.1371px, 281.3472px) rotate(-0.431052rad);
+  }
+
+  .warp__8 {
+    transform-origin: 50% 28px;
+    transform: translate(235.6262px, 271.2093px) rotate(-0.467732rad);
+  }
+
+  .warp__9 {
+    transform-origin: 50% 28px;
+    transform: translate(255.8562px, 260.3285px) rotate(-0.497897rad);
+  }
+
+  .warp__10 {
+    transform-origin: 50% 28px;
+    transform: translate(277.7371px, 250.92329999999998px) rotate(-0.518045rad);
+  }
+
+  .warp__11 {
+    transform-origin: 50% 28px;
+    transform: translate(289.2603px, 241.9142px) rotate(-0.533078rad);
+  }
+
+  .warp__12 {
+    transform-origin: 50% 28px;
+    transform: translate(308.1684px, 230.57029999999997px) rotate(-0.546826rad);
+  }
+
+  .warp__13 {
+    transform-origin: 50% 28px;
+    transform: translate(325.6847px, 217.6273px) rotate(-0.556092rad);
+  }
+
+  .warp__14 {
+    transform-origin: 50% 28px;
+    transform: translate(350.5813px, 203.9596px) rotate(-0.558707rad);
+  }
+
+  .warp__15 {
+    transform-origin: 50% 28px;
+    transform: translate(370.4287px, 191.9054px) rotate(-0.554627rad);
+  }
+
+  .warp__16 {
+    transform-origin: 50% 28px;
+    transform: translate(388.4318px, 180.8603px) rotate(-0.544964rad);
+  }
+
+  .warp__17 {
+    transform-origin: 50% 28px;
+    transform: translate(406.3458px, 169.5962px) rotate(-0.528069rad);
+  }
+
+  .warp__18 {
+    transform-origin: 50% 28px;
+    transform: translate(427.2597px, 160.7953px) rotate(-0.508596rad);
+  }
+
+  .warp__19 {
+    transform-origin: 50% 28px;
+    transform: translate(438.2696px, 154.2443px) rotate(-0.489466rad);
+  }
+
+  .warp__20 {
+    transform-origin: 50% 28px;
+    transform: translate(451.0063px, 145.9123px) rotate(-0.457449rad);
+  }
+
+  .warp__21 {
+    transform-origin: 50% 28px;
+    transform: translate(469.9713px, 135.6057px) rotate(-0.399791rad);
+  }
+
+  .warp__22 {
+    transform-origin: 50% 28px;
+    transform: translate(496.6821px, 126.80609999999999px) rotate(-0.321024rad);
+  }
+
+  .warp__23 {
+    transform-origin: 50% 28px;
+    transform: translate(517.004px, 120.8682px) rotate(-0.230915rad);
+  }
+
+  .warp__24 {
+    transform-origin: 50% 28px;
+    transform: translate(539.0688px, 117.1634px) rotate(-0.121775rad);
+  }
+
+  .warp__25 {
+    transform-origin: 50% 28px;
+    transform: translate(558.9029px, 115.9787px) rotate(0.019824rad);
+  }
+
+  .warp__26 {
+    transform-origin: 50% 28px;
+    transform: translate(583.4416px, 118.34360000000001px) rotate(0.187262rad);
+  }
+
+  .warp__27 {
+    transform-origin: 50% 28px;
+    transform: translate(605.4579px, 123.73079999999999px) rotate(0.199635rad);
+  }
+
+  .warp__28 {
+    transform-origin: 50% 28px;
+    transform: translate(627.4152px, 126.22800000000001px) rotate(0.06034rad);
+  }
+
+  .warp__29 {
+    transform-origin: 50% 28px;
+    transform: translate(643.8268px, 126.00380000000001px) rotate(-0.084317rad);
+  }
+
+  .warp__30 {
+    transform-origin: 50% 28px;
+    transform: translate(664.7977px, 121.6241px) rotate(-0.266675rad);
+  }
+
+  .warp__31 {
+    transform-origin: 50% 28px;
+    transform: translate(691.978px, 114.9916px) rotate(-0.395449rad);
+  }
+
+  .warp__32 {
+    transform-origin: 50% 28px;
+    transform: translate(702.4439px, 107.14250000000001px) rotate(-0.490912rad);
+  }
+
+  .warp__33 {
+    transform-origin: 50% 28px;
+    transform: translate(723.6024px, 95.3678px) rotate(-0.583512rad);
+  }
+
+  .warp__34 {
+    transform-origin: 50% 28px;
+    transform: translate(741.8204px, 85.0494px) rotate(-0.636706rad);
+  }
+
+  .warp__35 {
+    transform-origin: 50% 28px;
+    transform: translate(752.666px, 73.9834px) rotate(-0.674949rad);
+  }
+
+  .warp__36 {
+    transform-origin: 50% 28px;
+    transform: translate(769.4339px, 60.982600000000005px) rotate(-0.701171rad);
+  }
+
+  .warp__37 {
+    transform-origin: 50% 28px;
+    transform: translate(783.8907px, 47.7749px) rotate(-0.71017rad);
+  }
+
+  .warp__38 {
+    transform-origin: 50% 28px;
+    transform: translate(801.2518px, 37.60980000000001px) rotate(-0.705125rad);
+  }
+
+  .warp__39 {
+    transform-origin: 50% 28px;
+    transform: translate(807.6909px, 26.3656px) rotate(-0.685707rad);
+  }
+
+  .warp__40 {
+    transform-origin: 50% 28px;
+    transform: translate(827.0153px, 12.449399999999997px) rotate(-0.634541rad);
+  }
+
+  .warp__41 {
+    transform-origin: 50% 28px;
+    transform: translate(844.3614px, 0.36070000000000135px) rotate(-0.549224rad);
+  }
+
+  .warp__42 {
+    transform-origin: 50% 28px;
+    transform: translate(863.6942px, -12.0805px) rotate(-0.362854rad);
+  }
+
+  .warp__43 {
+    transform-origin: 50% 28px;
+    transform: translate(896.2573px, -17.826900000000002px) rotate(-0.123596rad);
+  }
+
+  /* h2 {
     text-align: center;
     padding: 20px;
   }
@@ -73,7 +362,7 @@
   .image {
     text-align: center;
     padding-bottom: 20px;
-  }
+  } */
   .wave {
     --mask: radial-gradient(52.5px at 50% calc(100% - 74px), #000 99%, #0000 101%) calc(50% - 80px) 0/160px 100%, radial-gradient(52.5px at 50% calc(100% + 34px), #0000 99%, #000 101%) 50% calc(100% - 40px) / 160px 100% repeat-x;
     mask: var(--mask);
